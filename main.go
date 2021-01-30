aa1edddbde91963703cce58f92e0a01332e07ef7
Michal Mildner <mm0070@gmail.com>
2021-01-30 13:22:48 +0000

    get model and photo link; update .gitignore

--- main.go
+++ main.go
@@ -3,13 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gocolly/colly/v2"
 )
 
-func main() {
+type question struct {
+	Manufacturer string
+	Model        string
+	PhotoURL     string
+	RawData      string
+}
+
+type questionSet struct {
+	question []question
+}
+
+func visit(photoID int) string {
+	var model string
 	c := colly.NewCollector(
-		// colly.AllowedDomains("www.jetphotos.net"),
+		colly.AllowedDomains("jetphotos.com", "www.jetphotos.com"),
 		colly.CacheDir("./colly_cache"),
 	)
 
@@ -17,18 +33,33 @@ func main() {
 		log.Println("Something went wrong:", err)
 	})
 
-	c.OnHTML(".list__item", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		if !strings.Contains(e.Text, "Latest aviation photos on JetPhotos") {
+			// fmt.Println(e.Text)
+			model = strings.Split(e.Text, "|")[1]
+		}
+	})
+
+	c.OnHTML("img.large-photo__img", func(e *colly.HTMLElement) {
+		fmt.Println(e)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
 	})
 
-	err := c.Visit("https://www.jetphotos.com/photo/10035336")
+	err := c.Visit("https://www.jetphotos.com/photo/" + strconv.Itoa(photoID))
 	if err != nil {
 		log.Println(err)
 	}
+	return model
 }
 
-// https://www.jetphotos.com/photo/10035336
+func main() {
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(visit(rand.Intn(9223337) + 1))
+		time.Sleep(time.Second * 1)
+	}
+
+}
