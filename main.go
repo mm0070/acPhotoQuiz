37e16c4e243b97c4c1748b28c677db9bc7dc200b
Michal Mildner <mm0070@gmail.com>
2021-01-31 10:32:57 +0000

    move question logic to questions package

--- main.go
+++ main.go
@@ -1,43 +1,12 @@
 package main
 
 import (
-	"log"
-	"math/rand"
-	"strings"
-	"time"
+	"fmt"
+
+	"github.com/mm0070/acPhotoQuiz/questions"
 )
 
-func prepareQuestions() []quizQuestion {
-	var returnedQuestions int
-	var questionArray []quizQuestion
-	var question quizQuestion
-	var data questionData
-	var match bool
-	numberOfQuestions := 10
-
-	for returnedQuestions < numberOfQuestions {
-		data = getPhotoInfo(rand.Intn(9223337) + 1)
-		question, match = matchAircraftTypes(&data)
-		if match == true {
-			questionArray = append(questionArray, question)
-			returnedQuestions++
-		}
-		// don't ddos jetphotos
-		time.Sleep(time.Second)
-	}
-
-	// debug lines
-	for _, v := range questionArray {
-		log.Println("")
-		log.Println("    Found manufacturer: ", v.Manufacturer)
-		log.Println("    Found model: ", v.Model)
-		log.Println("    PhotoURL: ", v.PhotoURL)
-		log.Println("    Jetphotos thinks this is: ", strings.Split(v.TitleRawData, "|")[1])
-	}
-
-	return questionArray
-}
-
 func main() {
-	prepareQuestions()
+	q := questions.PrepareQuestions()
+	fmt.Println(q)
 }
---
+++ questions/questions.go
@@ -0,0 +1,39 @@
+package questions
+
+import (
+	"log"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func PrepareQuestions() []quizQuestion {
+	var returnedQuestions int
+	var questionArray []quizQuestion
+	var question quizQuestion
+	var data questionData
+	var match bool
+	numberOfQuestions := 10
+
+	for returnedQuestions < numberOfQuestions {
+		data = getPhotoInfo(rand.Intn(9223337) + 1)
+		question, match = matchAircraftTypes(&data)
+		if match == true {
+			questionArray = append(questionArray, question)
+			returnedQuestions++
+		}
+		// don't ddos jetphotos
+		time.Sleep(time.Second)
+	}
+
+	// debug lines
+	for _, v := range questionArray {
+		log.Println("")
+		log.Println("    Found manufacturer: ", v.Manufacturer)
+		log.Println("    Found model: ", v.Model)
+		log.Println("    PhotoURL: ", v.PhotoURL)
+		log.Println("    Jetphotos thinks this is: ", strings.Split(v.TitleRawData, "|")[1])
+	}
+
+	return questionArray
+}
