146aadad2e3a78f5bc52b20a022191893baebe77
Michal Mildner <mm0070@gmail.com>
2021-02-07 22:51:41 +0000

    add ac list to vue, add bootstrap

--- quizapp/src/App.vue
+++ quizapp/src/App.vue
@@ -1,37 +1,43 @@
 <template>
   <div>
     <Loading v-if="questions.length < numberOfQuestions" :questions="questions"></Loading>
-    <!-- <Picture v-if="questions.length != 0" :imgSrc="questions[0].photo_url"></Picture> -->
+    <Picture v-if="questions.length == numberOfQuestions" :imgSrc="questions[currentQuestion].photo_url"></Picture>
+    <Selector></Selector>
   </div>
 </template>
 
 <script>
-// import Picture from './components/Picture.vue'
+import Picture from './components/Picture.vue'
 import axios from 'axios';
 import Loading from './components/Loading.vue';
+import Selector from './components/Selector.vue';
 
 export default {
   name: 'App',
   components: {
-    // Picture,
-    Loading
+    Picture,
+    Loading,
+    Selector
   },
   data() {
     return {
       questions: [], // array to hold questions returned from the API
       questionCount: 0, // count the number of questions returned while fetching - used for loading page
-      numberOfQuestions: 1, // number of questions to fetch
+      numberOfQuestions: 2, // number of questions to fetch
+      currentQuestion: 0,
     }
-  },
-  methods: {
   },
   mounted() {
     for (this.questionCount = 0; this.questionCount < this.numberOfQuestions; this.questionCount++) {
       axios.get("http://127.0.0.1:4000/getQuestions/test/1").then(response => this.questions.push(response.data.questions[0]));
     }
-    }
-  }
+  },
+  methods: {
+    nextQuestion() {
 
+    }
+  } 
+}
 </script>
 
 <style>
